d8315c385b14411e039f65f1293cb1ec07a0422a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-23 20:07:38 +0800

    Adding PSEUDO CODE.

---
+++ temperature_converter.py
@@ -0,0 +1,11 @@
+# ========== PSEUDO CODE ==========
+# || ACTUAL CODE ||
+# - Ask user to input a positive or negative float number for temperature.
+
+# - Formula of the conversion celsius to fahrenheit, vice versa
+
+
+# - Ask user user to select the conversion type: from Celsius to Fahrenheit or from Fahrenheit to Celsius.
+
+# - Conditional Statement below
+
